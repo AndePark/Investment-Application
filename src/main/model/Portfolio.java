ebe8752d3f4d89a39bde7c44f9006ca847b17dfc
andrewpark <andrewpark2396@gmail.com>
2022-02-08 10:55:20 -0800

    project

--- src/main/model/Portfolio.java
+++ src/main/model/Portfolio.java
@@ -1,17 +1,39 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Portfolio {
     private ArrayList<Invest> investments;
+    private HashMap<String, Invest> portfolio;
 
+
+    //REQUIRES:
+    //MODIFIES:
+    //EFFECTS: constructs an empty hashmap with key being stock ticker and investments being invest objects associated
+    //         with that exact ticker
     public Portfolio() {
         investments = new ArrayList<>();
+        portfolio = new HashMap<String, Invest>();
     }
 
-    public void addInvestment(Invest invest) {
-        investments.add(invest);
+
+    //REQUIRES:
+    //MODIFIES: this
+    //EFFECTS: if HashMap doesn't contain given string as a key, then create new hashmap key-val pair. Otherwise,
+    //         add the new invest object to existing key-val pair
+    public void addToPortfolio(String name, double listedPrice, double amountFunded) {
+        if()
+
+
+        }
+
     }
+
+
+
+
+
 
     public void sellInvestment(Invest invest) {
         investments.
